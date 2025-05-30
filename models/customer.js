552125da4e60268b883793126fed471c9c11f254
etinyene ededem <etinyeneededem07@gmail.com>
2025-05-30 09:54:32 +0100

    bugfix on customer contacts

--- models/customer.js
+++ models/customer.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
-
+const Counter = require("./counter");
 const { Schema } = mongoose;
+
+function padNumber(num, length) {
+  return num.toString().padStart(length, "0");
+}
 
 const contactSchema = new Schema({
   name: { type: String },
   city: { type: String },
   country: { type: String },
   email: { type: String },
-  companyDomain: { type: String, required: true },
   phoneNumber: { type: String },
 });
 
 const customerSchema = new Schema({
+  customerNo:{ type: String},
   type: { type: String, required: true },
   category: { type: String, required: true },
   name: { type: String, required: true },
@@ -23,6 +27,25 @@ const customerSchema = new Schema({
   country: { type: String, required: true },
   customerPhoto: { type: String },
   contacts: { type: [contactSchema] },
+});
+
+// eslint-disable-next-line func-names
+customerSchema.pre("validate", async function (next) {
+  try {
+    console.log({ companyDomain: this.companyDomain });
+    const counter = await Counter.findOneAndUpdate(
+      { counterId: "customerNo", companyDomain: this.companyDomain },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+    const currentYear = new Date().getFullYear();
+    const lastTwoDigits = currentYear.toString().slice(-2);
+    const currentMonth = new Date().getMonth();
+    this.customerNo = `COY-${currentMonth}-${lastTwoDigits}-${padNumber(counter.seq, 4)}`;
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = mongoose.model("Customer", customerSchema);
--- routes/customer.js
+++ routes/customer.js
@@ -5,11 +5,14 @@ const multer = require('../middlewares/multer-config');
 const router = express.Router();
 const customerCtrl = require('../controllers/customer');
 
+
 router.get('/', auth, customerCtrl.getCustomers);
 router.get('/:id', auth, customerCtrl.getCustomer);
 router.post('/', auth, customerCtrl.addCustomer);
 router.delete('/', auth, customerCtrl.deleteCustomer);
 router.patch('/:id', auth, multer, customerCtrl.updateCustomer);
 router.delete('/contacts/:id', auth, customerCtrl.deleteContacts);
+router.patch('/:customerId/contacts/:contactId', auth, customerCtrl.updateCustomerContact);
+
 
 module.exports = router;
